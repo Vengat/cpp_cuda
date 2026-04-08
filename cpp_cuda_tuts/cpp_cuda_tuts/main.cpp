c5882ce229a84278f4b0e2d984f46bc140f3a806
Vengat <vengatramanan@gmail.com>
2026-04-08 16:19:43 +0530

    first commit

---
+++ cpp_cuda_tuts/cpp_cuda_tuts/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_cuda_tuts
+//
+//  Created by Vengat Ramanan on 08/04/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
